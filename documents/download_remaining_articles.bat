051d524581be6f0b0e5b2660985230875271c427
David-Saeteros <david94saeteros@gmail.com>
2025-03-05 20:09:59 +0100

    Files produced by the check_download.py file. It stores all the articles that need manual downloading.

---
+++ documents/download_remaining_articles.bat
@@ -0,0 +1,116 @@
+@echo off
+echo Starting download of remaining articles...
+python -m PyPaperBot --doi="10.1037/prj0000303" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1111/inm.13007" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1186/s12889-017-4329-1" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1037/prj0000556" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.2196/jmir.4097" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1037/prj0000518" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1037/ser0000635" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.20230408" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s00420-023-02039-7" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1017/S0033291723003744" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10926-022-10046-7" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10488-023-01326-2" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1016/j.pmedr.2024.102786" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1177/00207640231217177" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10488-021-01174-y" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1177/00207640221104684" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1016/j.cct.2023.107342" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.5093/pi2018a2" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202000926" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1016/j.schres.2021.09.001" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1017/S1463423621000827" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1371/journal.pone.0304527" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202100516" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.5271/sjweh.3753" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1111/spol.12968" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.2196/mental.6982" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1192/j.eurpsy.2021.25" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1186/s12888-022-03881-8" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1177/0193841X211049685" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1037/prj0000589" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1192/j.eurpsy.2022.2309" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10488-022-01228-9" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.20230023" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.5014/ajot.2018.033068" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1186/s12888-021-03416-7" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1055/a-2165-8728" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.13075/ijomeh.1896.01074" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.20230597" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10488-021-01180-0" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1080/11038128.2023.2200576" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1016/j.schres.2023.11.003" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202100199" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202000671" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.3233/JVR-230045" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.3389/fpsyt.2021.709732" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1192/j.eurpsy.2023.697" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.3368/jhr.58.4.0319-10105R2" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.36148/2284-0249-346" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1136/bmjopen-2021-049522" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202100197" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1007/s10775-022-09533-3" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1111/hsc.13892" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1186/s13063-018-2485-y" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1016/j.jhealeco.2024.102958" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1186/s13063-023-07462-2" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+python -m PyPaperBot --doi="10.1176/appi.ps.202000649" --dwn-dir="c:\Users\david.gallardo\OneDrive - Universitat de Barcelona\GitHub\SMART_reviews\SMART-Review\documents\articles" --use-doi-as-filename
+echo.
+echo All downloads completed.
+pause
